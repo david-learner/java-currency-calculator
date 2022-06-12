ca3430778dddf25b4821278209c018bb74d73db2
david <xodnjs2549@naver.com>
2022-06-12 14:07:25 +0900

    first(project): make project

---
+++ src/main/java/david/currencycalculator/JavaCurrencyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package david.currencycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCurrencyCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCurrencyCalculatorApplication.class, args);
+    }
+
+}
