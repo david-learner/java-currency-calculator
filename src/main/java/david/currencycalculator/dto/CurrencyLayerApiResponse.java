d821fb31021db27df0ed7c4db560c94b2110a0be
david <xodnjs2549@naver.com>
2022-06-13 10:06:43 +0900

    test(CurrencyApiController): validate currency quotation data from currency layer service

---
+++ src/main/java/david/currencycalculator/dto/CurrencyLayerApiResponse.java
@@ -0,0 +1,31 @@
+package david.currencycalculator.dto;
+
+import java.util.Map;
+
+public class CurrencyLayerApiResponse {
+
+    private boolean success;
+    private long timestamp;
+    private String source;
+    private Map<String, Object> quotes;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public Map<String, Object> getQuotes() {
+        return quotes;
+    }
+
+    public boolean hasQuote(String quote) {
+        return quotes.containsKey(quote);
+    }
+}
---
+++ src/test/java/david/currencycalculator/api/CurrencyApiControllerTest.java
@@ -0,0 +1,34 @@
+package david.currencycalculator.api;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import david.currencycalculator.dto.CurrencyLayerApiResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class CurrencyApiControllerTest {
+
+    @Test
+    @DisplayName("api layer 서비스에 한국, 일본, 필리핀 환율 정보를 요청한다")
+    void Request_korea_japan_philippines_currency_data_from_api_layer_service() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("apikey", "5m6uwmrQMERAOCMlHeai8jvOgR10x8zr");
+        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+        String url = "https://api.apilayer.com/currency_data/live?source=USD&currencies=KRW%2CJPY%2CPHP";
+
+        ResponseEntity<CurrencyLayerApiResponse> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity,
+                CurrencyLayerApiResponse.class);
+
+        CurrencyLayerApiResponse currencyLayerApiResponse = response.getBody();
+        assertThat(currencyLayerApiResponse.isSuccess()).isTrue();
+        assertThat(currencyLayerApiResponse.hasQuote("USDKRW")).isTrue();
+        assertThat(currencyLayerApiResponse.hasQuote("USDJPY")).isTrue();
+        assertThat(currencyLayerApiResponse.hasQuote("USDPHP")).isTrue();
+    }
+}
